c3e6b34ea30539bab3d2a4107d17fbe2ff2acffb
ando-hunterz <ando.hunterz@gmail.com>
2021-05-04 12:07:13 +0700

    fix url and cookies

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -4,20 +4,20 @@ const auth = (state = {authData : null}, action) => {
     switch(action.type){
         case 'SIGNIN' : 
             // localStorage.setItem('profile', JSON.stringify({...action.payload}))
-            Cookies.set('db_id', action.payload.db_id, { path: '/' })
-            Cookies.set('jwtToken', action.payload.jwtToken, { path: '/' })
+            Cookies.set('db_id', action.payload.db_id, { path: '/', secure: true, sameSite: 'none' })
+            Cookies.set('jwtToken', action.payload.jwtToken, { path: '/', secure: true, sameSite: 'none' })
             return {...state, authData: action.payload};
 
         case 'SIGNUP' : 
             // localStorage.setItem('profile', JSON.stringify({...action.payload}))
-            Cookies.set('db_id', action.payload.db_id, { path: '/' })
-            Cookies.set('jwtToken', action.payload.jwtToken, { path: '/' })
+            Cookies.set('db_id', action.payload.db_id, { path: '/', secure: true, sameSite: 'none' })
+            Cookies.set('jwtToken', action.payload.jwtToken, { path: '/', secure: true, sameSite: 'none' })
             return {...state, authData: action.payload};
 
         case 'LOGOUT' : 
             // localStorage.clear()
-            Cookies.remove('db_id', {path: '/'})
-            Cookies.remove('jwtToken', {path: '/'})
+            Cookies.remove('db_id', {path: '/', secure: true, sameSite: 'none'})
+            Cookies.remove('jwtToken', {path: '/', secure: true, sameSite: 'none'})
             return {...state, authData: null};
             
         default : return state;
